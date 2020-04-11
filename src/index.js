b800a7c835a26357c0c82d800060c1b78f6cfc03
Anshuman Singh <39101145+anshuman-aaa@users.noreply.github.com>
2020-04-11 23:45:35 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+require("./Db/mongoose");
+//main dashboard routers
+
+const app = express();
+const port = process.env.PORT;
+var cors = require("cors");
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log("Server isup on port " + port);
+});
+// pid=50410 port=27017
+// https://git.heroku.com/shopify-api-nodejs.git
+// https://shopify-api-nodejs.herokuapp.com/
